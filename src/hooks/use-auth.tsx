a2333a8052290296bef7c99b74427f9c02b38059
Galí Estudio <galiestudiodev@gmail.com>
2025-09-15 18:43:37 +0000

    esta el la respuesta cuando se consulta el alojamiento, no se porque arr

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -93,12 +93,11 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const login = (jwt: string, userData: User, accommodationData: any[]) => {
     const processedAccommodations = accommodationData.map(item => {
         const { id, attributes } = item;
-        const planData = attributes.plan?.data;
         return {
             id: id,
             documentId: String(id),
             ...attributes,
-            plan: planData ? { id: planData.id, ...planData.attributes } : null,
+            plan: attributes.plan,
         };
     });
     
